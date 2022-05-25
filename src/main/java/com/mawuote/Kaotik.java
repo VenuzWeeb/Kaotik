5c2a619db9a36244aae315deca3b48e06ecda559
SomeSadKid_ <104260285+somesadkidontop@users.noreply.github.com>
2022-05-24 19:58:48 -0500

    Bumped up to 1.3.3

--- src/main/java/com/mawuote/Kaotik.java
+++ src/main/java/com/mawuote/Kaotik.java
@@ -32,7 +32,7 @@ import java.util.Arrays;
 @Mod(modid = Kaotik.MODID, name = Kaotik.NAME, version = Kaotik.VERSION)
 public class Kaotik {
     public static final String NAME = "Kaotik";
-    public static final String VERSION = "1.3.2";
+    public static final String VERSION = "1.3.3";
     public static final String MODID = "kaotik";
 
     public static final Logger LOGGER = LogManager.getLogger("Kaotik");
@@ -89,7 +89,7 @@ public class Kaotik {
     public void postInitialize(FMLPostInitializationEvent event){
         LOGGER.info("Started Post-Initialization process for Kaotik v" + VERSION + "!");
 
-        Display.setTitle(Kaotik.NAME + " " + Kaotik.VERSION + " | kaotik got evolved");
+        Display.setTitle(Kaotik.NAME + " " + Kaotik.VERSION + " | Kaotik Continued");
 
         LOGGER.info("Finished Post-Initialization process for Kaotik v" + VERSION + "!");
     }
